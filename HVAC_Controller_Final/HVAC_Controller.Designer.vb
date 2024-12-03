a4bc13e4c079c53751afcb30e4265857eace041f
Rahiel Rodriguez <rahielrr@gmail.com>
2024-12-02 21:00:17 -0700

    Setpoint Controllers added

--- HVAC_Controller_Final/HVAC_Controller.Designer.vb
+++ HVAC_Controller_Final/HVAC_Controller.Designer.vb
@@ -22,10 +22,103 @@ Partial Class HVAC_Controller
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.HighTemp1Button = New System.Windows.Forms.Button()
+        Me.HighTemp2Button = New System.Windows.Forms.Button()
+        Me.CoolingTemp1Button = New System.Windows.Forms.Button()
+        Me.CoolingTemp2Button = New System.Windows.Forms.Button()
+        Me.HeaterSetpointTextBox = New System.Windows.Forms.TextBox()
+        Me.CoolingSetpointTextBox = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'HighTemp1Button
+        '
+        Me.HighTemp1Button.Location = New System.Drawing.Point(290, 259)
+        Me.HighTemp1Button.Name = "HighTemp1Button"
+        Me.HighTemp1Button.Size = New System.Drawing.Size(94, 50)
+        Me.HighTemp1Button.TabIndex = 0
+        Me.HighTemp1Button.Text = "Heater Setpoint ^"
+        Me.HighTemp1Button.UseVisualStyleBackColor = True
+        '
+        'HighTemp2Button
+        '
+        Me.HighTemp2Button.Location = New System.Drawing.Point(290, 315)
+        Me.HighTemp2Button.Name = "HighTemp2Button"
+        Me.HighTemp2Button.Size = New System.Drawing.Size(94, 50)
+        Me.HighTemp2Button.TabIndex = 1
+        Me.HighTemp2Button.Text = "Heater Setpoint -"
+        Me.HighTemp2Button.UseVisualStyleBackColor = True
+        '
+        'CoolingTemp1Button
+        '
+        Me.CoolingTemp1Button.Location = New System.Drawing.Point(408, 259)
+        Me.CoolingTemp1Button.Name = "CoolingTemp1Button"
+        Me.CoolingTemp1Button.Size = New System.Drawing.Size(94, 50)
+        Me.CoolingTemp1Button.TabIndex = 3
+        Me.CoolingTemp1Button.Text = "Cooling Setpoint  ^"
+        Me.CoolingTemp1Button.UseVisualStyleBackColor = True
+        '
+        'CoolingTemp2Button
+        '
+        Me.CoolingTemp2Button.Location = New System.Drawing.Point(408, 315)
+        Me.CoolingTemp2Button.Name = "CoolingTemp2Button"
+        Me.CoolingTemp2Button.Size = New System.Drawing.Size(94, 50)
+        Me.CoolingTemp2Button.TabIndex = 2
+        Me.CoolingTemp2Button.Text = "Cooling Setpoint -"
+        Me.CoolingTemp2Button.UseVisualStyleBackColor = True
+        '
+        'HeaterSetpointTextBox
+        '
+        Me.HeaterSetpointTextBox.Enabled = False
+        Me.HeaterSetpointTextBox.Location = New System.Drawing.Point(284, 231)
+        Me.HeaterSetpointTextBox.Name = "HeaterSetpointTextBox"
+        Me.HeaterSetpointTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.HeaterSetpointTextBox.TabIndex = 4
+        '
+        'CoolingSetpointTextBox
+        '
+        Me.CoolingSetpointTextBox.Enabled = False
+        Me.CoolingSetpointTextBox.Location = New System.Drawing.Point(402, 231)
+        Me.CoolingSetpointTextBox.Name = "CoolingSetpointTextBox"
+        Me.CoolingSetpointTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.CoolingSetpointTextBox.TabIndex = 5
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Silver
+        Me.PictureBox1.Location = New System.Drawing.Point(332, 84)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(118, 111)
+        Me.PictureBox1.TabIndex = 6
+        Me.PictureBox1.TabStop = False
+        '
+        'HVAC_Controller
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.CoolingSetpointTextBox)
+        Me.Controls.Add(Me.HeaterSetpointTextBox)
+        Me.Controls.Add(Me.CoolingTemp1Button)
+        Me.Controls.Add(Me.CoolingTemp2Button)
+        Me.Controls.Add(Me.HighTemp2Button)
+        Me.Controls.Add(Me.HighTemp1Button)
+        Me.Name = "HVAC_Controller"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents HighTemp1Button As Button
+    Friend WithEvents HighTemp2Button As Button
+    Friend WithEvents CoolingTemp1Button As Button
+    Friend WithEvents CoolingTemp2Button As Button
+    Friend WithEvents HeaterSetpointTextBox As TextBox
+    Friend WithEvents CoolingSetpointTextBox As TextBox
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
--- HVAC_Controller_Final/HVAC_Controller.vb
+++ HVAC_Controller_Final/HVAC_Controller.vb
@@ -1,3 +1,44 @@
 ﻿Public Class HVAC_Controller
+    Dim heaterSetpoint As Double = 75
+    Dim coolingSetpoint As Double = 60
+    Private Sub HighTemp1Button_Click(sender As Object, e As EventArgs) Handles HighTemp1Button.Click
+        If heaterSetpoint < 90 Then
+            heaterSetpoint += 0.5
+            HeaterSetpointTextBox.Text = $"{heaterSetpoint}"
+        Else
+            MsgBox("Setpoints can go from 50 to 90 degrees. Please, select a valid value within range.", MsgBoxStyle.Exclamation, "Setpoint Error")
+        End If
+    End Sub
 
+    Private Sub HighTemp2Button_Click(sender As Object, e As EventArgs) Handles HighTemp2Button.Click
+        If heaterSetpoint > 50 Then
+            heaterSetpoint -= 0.5
+            HeaterSetpointTextBox.Text = $"{heaterSetpoint}"
+        Else
+            MsgBox("Setpoints can go from 50 to 90 degrees. Please, select a valid value within range.", MsgBoxStyle.Exclamation, "Setpoint Error")
+        End If
+    End Sub
+
+    Private Sub CoolingTemp1Button_Click(sender As Object, e As EventArgs) Handles CoolingTemp1Button.Click
+        If coolingSetpoint < 90 Then
+            coolingSetpoint += 0.5
+            CoolingSetpointTextBox.Text = $"{coolingSetpoint}"
+        Else
+            MsgBox("Setpoints can go from 50 to 90 degrees. Please, select a valid value within range.", MsgBoxStyle.Exclamation, "Setpoint Error")
+        End If
+    End Sub
+
+    Private Sub CoolingTemp2Button_Click(sender As Object, e As EventArgs) Handles CoolingTemp2Button.Click
+        If coolingSetpoint > 50 Then
+            coolingSetpoint -= 0.5
+            CoolingSetpointTextBox.Text = $"{coolingSetpoint}"
+        Else
+            MsgBox("Setpoints can go from 50 to 90 degrees. Please, select a valid value within range.", MsgBoxStyle.Exclamation, "Setpoint Error")
+        End If
+    End Sub
+
+    Private Sub HVAC_Controller_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        HeaterSetpointTextBox.Text = $"{heaterSetpoint}"
+        CoolingSetpointTextBox.Text = $"{coolingSetpoint}"
+    End Sub
 End Class
