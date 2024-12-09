f5534c584067ff7d6f2bf55a84dd16ed58ff9557
Rahiel Rodriguez <rahielrr@gmail.com>
2024-12-09 16:18:22 -0700

    Digital and analog control added Alarm, HVAC and Fan Picture Boxes added

--- HVAC_Controller_Final/HVAC_Controller.Designer.vb
+++ HVAC_Controller_Final/HVAC_Controller.Designer.vb
@@ -29,13 +29,26 @@ Partial Class HVAC_Controller
         Me.CoolingTemp2Button = New System.Windows.Forms.Button()
         Me.HeaterSetpointTextBox = New System.Windows.Forms.TextBox()
         Me.CoolingSetpointTextBox = New System.Windows.Forms.TextBox()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.AlertPictureBox = New System.Windows.Forms.PictureBox()
         Me.QYBoardSerialPort = New System.IO.Ports.SerialPort(Me.components)
         Me.PortsComboBox = New System.Windows.Forms.ComboBox()
         Me.ComButton = New System.Windows.Forms.Button()
         Me.ClockTimer = New System.Windows.Forms.Timer(Me.components)
         Me.ClockLabel = New System.Windows.Forms.Label()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.RoomTempLabel = New System.Windows.Forms.Label()
+        Me.SystemTempLabel = New System.Windows.Forms.Label()
+        Me.TempTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.HeaterRadioButton = New System.Windows.Forms.RadioButton()
+        Me.ACRadioButton = New System.Windows.Forms.RadioButton()
+        Me.OffRadioButton = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.HVACPictureBox = New System.Windows.Forms.PictureBox()
+        Me.FanTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.FanPictureBox = New System.Windows.Forms.PictureBox()
+        CType(Me.AlertPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.HVACPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.FanPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'HighTemp1Button
@@ -90,14 +103,14 @@ Partial Class HVAC_Controller
         Me.CoolingSetpointTextBox.Size = New System.Drawing.Size(100, 22)
         Me.CoolingSetpointTextBox.TabIndex = 5
         '
-        'PictureBox1
+        'AlertPictureBox
         '
-        Me.PictureBox1.BackColor = System.Drawing.Color.Silver
-        Me.PictureBox1.Location = New System.Drawing.Point(332, 84)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(118, 111)
-        Me.PictureBox1.TabIndex = 6
-        Me.PictureBox1.TabStop = False
+        Me.AlertPictureBox.BackColor = System.Drawing.Color.Silver
+        Me.AlertPictureBox.Location = New System.Drawing.Point(12, 42)
+        Me.AlertPictureBox.Name = "AlertPictureBox"
+        Me.AlertPictureBox.Size = New System.Drawing.Size(29, 26)
+        Me.AlertPictureBox.TabIndex = 6
+        Me.AlertPictureBox.TabStop = False
         '
         'PortsComboBox
         '
@@ -122,32 +135,130 @@ Partial Class HVAC_Controller
         '
         'ClockLabel
         '
+        Me.ClockLabel.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.ClockLabel.AutoSize = True
-        Me.ClockLabel.Location = New System.Drawing.Point(716, 9)
+        Me.ClockLabel.Location = New System.Drawing.Point(737, 9)
         Me.ClockLabel.Name = "ClockLabel"
         Me.ClockLabel.Size = New System.Drawing.Size(41, 16)
         Me.ClockLabel.TabIndex = 9
         Me.ClockLabel.Text = "Clock"
         '
+        'RoomTempLabel
+        '
+        Me.RoomTempLabel.AutoSize = True
+        Me.RoomTempLabel.Location = New System.Drawing.Point(681, 157)
+        Me.RoomTempLabel.Name = "RoomTempLabel"
+        Me.RoomTempLabel.Size = New System.Drawing.Size(83, 16)
+        Me.RoomTempLabel.TabIndex = 10
+        Me.RoomTempLabel.Text = "Room Temp"
+        '
+        'SystemTempLabel
+        '
+        Me.SystemTempLabel.AutoSize = True
+        Me.SystemTempLabel.Location = New System.Drawing.Point(681, 179)
+        Me.SystemTempLabel.Name = "SystemTempLabel"
+        Me.SystemTempLabel.Size = New System.Drawing.Size(91, 16)
+        Me.SystemTempLabel.TabIndex = 11
+        Me.SystemTempLabel.Text = "System Temp"
+        '
+        'TempTimer
+        '
+        Me.TempTimer.Interval = 50
+        '
+        'HeaterRadioButton
+        '
+        Me.HeaterRadioButton.AutoSize = True
+        Me.HeaterRadioButton.Location = New System.Drawing.Point(6, 21)
+        Me.HeaterRadioButton.Name = "HeaterRadioButton"
+        Me.HeaterRadioButton.Size = New System.Drawing.Size(69, 20)
+        Me.HeaterRadioButton.TabIndex = 12
+        Me.HeaterRadioButton.Text = "Heater"
+        Me.HeaterRadioButton.UseVisualStyleBackColor = True
+        '
+        'ACRadioButton
+        '
+        Me.ACRadioButton.AutoSize = True
+        Me.ACRadioButton.Location = New System.Drawing.Point(6, 47)
+        Me.ACRadioButton.Name = "ACRadioButton"
+        Me.ACRadioButton.Size = New System.Drawing.Size(50, 20)
+        Me.ACRadioButton.TabIndex = 13
+        Me.ACRadioButton.Text = "A/C"
+        Me.ACRadioButton.UseVisualStyleBackColor = True
+        '
+        'OffRadioButton
+        '
+        Me.OffRadioButton.AutoSize = True
+        Me.OffRadioButton.Checked = True
+        Me.OffRadioButton.Location = New System.Drawing.Point(6, 73)
+        Me.OffRadioButton.Name = "OffRadioButton"
+        Me.OffRadioButton.Size = New System.Drawing.Size(44, 20)
+        Me.OffRadioButton.TabIndex = 14
+        Me.OffRadioButton.TabStop = True
+        Me.OffRadioButton.Text = "Off"
+        Me.OffRadioButton.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.OffRadioButton)
+        Me.GroupBox1.Controls.Add(Me.ACRadioButton)
+        Me.GroupBox1.Controls.Add(Me.HeaterRadioButton)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 315)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(84, 104)
+        Me.GroupBox1.TabIndex = 15
+        Me.GroupBox1.TabStop = False
+        '
+        'HVACPictureBox
+        '
+        Me.HVACPictureBox.BackColor = System.Drawing.Color.Silver
+        Me.HVACPictureBox.Location = New System.Drawing.Point(335, 88)
+        Me.HVACPictureBox.Name = "HVACPictureBox"
+        Me.HVACPictureBox.Size = New System.Drawing.Size(117, 107)
+        Me.HVACPictureBox.TabIndex = 16
+        Me.HVACPictureBox.TabStop = False
+        '
+        'FanTimer
+        '
+        Me.FanTimer.Interval = 5000
+        '
+        'FanPictureBox
+        '
+        Me.FanPictureBox.BackColor = System.Drawing.Color.Silver
+        Me.FanPictureBox.Location = New System.Drawing.Point(458, 169)
+        Me.FanPictureBox.Name = "FanPictureBox"
+        Me.FanPictureBox.Size = New System.Drawing.Size(29, 26)
+        Me.FanPictureBox.TabIndex = 17
+        Me.FanPictureBox.TabStop = False
+        '
         'HVAC_Controller
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(801, 447)
+        Me.Controls.Add(Me.FanPictureBox)
+        Me.Controls.Add(Me.HVACPictureBox)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.SystemTempLabel)
+        Me.Controls.Add(Me.RoomTempLabel)
         Me.Controls.Add(Me.ClockLabel)
         Me.Controls.Add(Me.ComButton)
         Me.Controls.Add(Me.PortsComboBox)
-        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.AlertPictureBox)
         Me.Controls.Add(Me.CoolingSetpointTextBox)
         Me.Controls.Add(Me.HeaterSetpointTextBox)
         Me.Controls.Add(Me.CoolingTemp1Button)
         Me.Controls.Add(Me.CoolingTemp2Button)
         Me.Controls.Add(Me.HighTemp2Button)
         Me.Controls.Add(Me.HighTemp1Button)
+        Me.ForeColor = System.Drawing.Color.Black
         Me.Name = "HVAC_Controller"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Form1"
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.AlertPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.HVACPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.FanPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -159,10 +270,20 @@ Partial Class HVAC_Controller
     Friend WithEvents CoolingTemp2Button As Button
     Friend WithEvents HeaterSetpointTextBox As TextBox
     Friend WithEvents CoolingSetpointTextBox As TextBox
-    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents AlertPictureBox As PictureBox
     Friend WithEvents QYBoardSerialPort As IO.Ports.SerialPort
     Friend WithEvents PortsComboBox As ComboBox
     Friend WithEvents ComButton As Button
     Friend WithEvents ClockTimer As Timer
     Friend WithEvents ClockLabel As Label
+    Friend WithEvents RoomTempLabel As Label
+    Friend WithEvents SystemTempLabel As Label
+    Friend WithEvents TempTimer As Timer
+    Friend WithEvents HeaterRadioButton As RadioButton
+    Friend WithEvents ACRadioButton As RadioButton
+    Friend WithEvents OffRadioButton As RadioButton
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents HVACPictureBox As PictureBox
+    Friend WithEvents FanTimer As Timer
+    Friend WithEvents FanPictureBox As PictureBox
 End Class
--- HVAC_Controller_Final/HVAC_Controller.vb
+++ HVAC_Controller_Final/HVAC_Controller.vb
@@ -1,10 +1,18 @@
 ﻿Imports System.Drawing.Text
 Imports System.IO.Ports
+Imports System.Threading.Thread
+Imports System.Windows.Forms.ComponentModel.Com2Interop
+Imports System.Runtime.InteropServices
 
 Public Class HVAC_Controller
-    Dim heaterSetpoint As Double = 75
-    Dim coolingSetpoint As Double = 60
-    '--------------------------------------------CLOCK------------------------------------------------------------
+    Dim heaterSetpoint As Double = 70
+    Dim coolingSetpoint As Double = 80
+    Dim roomTemp As Integer
+    Dim systemTemp As Integer
+    Dim overwriteHeater As Integer = 0
+    Dim overwriteAC As Integer = 0
+    Dim fanCycle As Integer = 0
+    '------------------------------------------------------------GUI Configuration---------------------------------------------------------------------------
     Sub newFont()
         Dim customFont As New PrivateFontCollection
         customFont.AddFontFile("C:\Users\Rahi\OneDrive\Desktop\ISU\Robotics\5th Semester\Programing\HVAC_Controller_Final\RobotoSlab-VariableFont_wght.ttf")
@@ -29,18 +37,41 @@ Public Class HVAC_Controller
         'add all available ports to the port combobox
         PortsComboBox.Items.Clear()
         For Each s As String In SerialPort.GetPortNames()
-            PortsComboBox.Items.Add($"{s}")
-        Next
+            If s = "COM3" Then
 
+            ElseIf s = "COM4" Then
+
+            Else
+                PortsComboBox.Items.Add($"{s}")
+            End If
+        Next
         PortsComboBox.SelectedIndex = 0
     End Sub
+    '----------------------------------------------------------------QY@ BOARD Functions----------------------------------------------------------------
+    Sub Qy_ReadDigitalInputs()
+        Dim data(0) As Byte
+        data(0) = &B110000
+        QYBoardSerialPort.Write(data, 0, 1)
+    End Sub
+    Sub Qy_ReadAnalogInPutA1()
+        Dim data(0) As Byte
+        data(0) = &B1010001
+        QYBoardSerialPort.Write(data, 0, 1)
+    End Sub
+    Sub Qy_ReadAnalogInPutA2()
+        Dim data(0) As Byte
+        data(0) = &B1010010
+        QYBoardSerialPort.Write(data, 0, 1)
+    End Sub
+
+    '-----------------------------------------------------------Buttons and Form Events-----------------------------------------------------------
     Private Sub ComButton_Click(sender As Object, e As EventArgs) Handles ComButton.Click
         'GetComPorts()
         GetPorts()
     End Sub
 
     Private Sub HighTemp1Button_Click(sender As Object, e As EventArgs) Handles HighTemp1Button.Click
-        If heaterSetpoint < 90 Then
+        If heaterSetpoint <= 90 Then
             heaterSetpoint += 0.5
             HeaterSetpointTextBox.Text = $"{heaterSetpoint}"
         Else
@@ -49,7 +80,7 @@ Public Class HVAC_Controller
     End Sub
 
     Private Sub HighTemp2Button_Click(sender As Object, e As EventArgs) Handles HighTemp2Button.Click
-        If heaterSetpoint > 50 Then
+        If heaterSetpoint >= 50 Then
             heaterSetpoint -= 0.5
             HeaterSetpointTextBox.Text = $"{heaterSetpoint}"
         Else
@@ -58,7 +89,7 @@ Public Class HVAC_Controller
     End Sub
 
     Private Sub CoolingTemp1Button_Click(sender As Object, e As EventArgs) Handles CoolingTemp1Button.Click
-        If coolingSetpoint < 90 Then
+        If coolingSetpoint <= 90 Then
             coolingSetpoint += 0.5
             CoolingSetpointTextBox.Text = $"{coolingSetpoint}"
         Else
@@ -67,7 +98,7 @@ Public Class HVAC_Controller
     End Sub
 
     Private Sub CoolingTemp2Button_Click(sender As Object, e As EventArgs) Handles CoolingTemp2Button.Click
-        If coolingSetpoint > 50 Then
+        If coolingSetpoint >= 50 Then
             coolingSetpoint -= 0.5
             CoolingSetpointTextBox.Text = $"{coolingSetpoint}"
         Else
@@ -82,6 +113,7 @@ Public Class HVAC_Controller
         GetPorts()
         ClockLabel.Text = DateTime.Now.ToString("hh:mm:ss")
         ClockTimer.Start()
+        TempTimer.Start()
     End Sub
 
     Private Sub PortsComboBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles PortsComboBox.SelectedIndexChanged
@@ -90,6 +122,111 @@ Public Class HVAC_Controller
     '--------------------------------------------CLOCK------------------------------------------------------------
     Private Sub ClockTimer_Tick(sender As Object, e As EventArgs) Handles ClockTimer.Tick
         ClockLabel.Text = DateTime.Now.ToString("hh:mm:ss")
+    End Sub
+
+    Private Sub TempTimer_Tick(sender As Object, e As EventArgs) Handles TempTimer.Tick
+        Dim dataA1(QYBoardSerialPort.BytesToRead) As Byte
+        Dim dataA2(QYBoardSerialPort.BytesToRead) As Byte
+        Dim dataDigital(QYBoardSerialPort.BytesToRead) As Byte
+        Qy_ReadAnalogInPutA1()
+        Sleep(5)
+
+        ReDim dataA1(QYBoardSerialPort.BytesToRead)
+        QYBoardSerialPort.Read(dataA1, 0, QYBoardSerialPort.BytesToRead)
+        RoomTempLabel.Text = $"{CInt((dataA1(0) / 6.4) + 50)} °F"
+        roomTemp = CInt((dataA1(0) / 6.4) + 50)
+        Qy_ReadAnalogInPutA2()
+        Sleep(5)
+
+        ReDim dataA2(QYBoardSerialPort.BytesToRead)
+        QYBoardSerialPort.Read(dataA2, 0, QYBoardSerialPort.BytesToRead)
+        SystemTempLabel.Text = $"{CInt((dataA2(0) / 6.4) + 50)} °F"
+        systemTemp = CInt((dataA2(0) / 6.4) + 50)
+        Qy_ReadDigitalInputs()
+        Sleep(5)
+
+        ReDim dataDigital(QYBoardSerialPort.BytesToRead)
+        QYBoardSerialPort.Read(dataDigital, 0, QYBoardSerialPort.BytesToRead)
+
+        If dataDigital(0) = 254 Then
+            OffRadioButton.Checked = True
+            FanPictureBox.BackColor = Color.Gray
+            HVACPictureBox.BackColor = Color.Gray
+            AlertPictureBox.BackColor = Color.Red
+        ElseIf dataDigital(0) = 253 Then
+            If fanCycle = 0 Then
+                FanPictureBox.BackColor = Color.Lime
+            Else
+                FanPictureBox.BackColor = Color.Gray
+            End If
+            overwriteHeater = 1
+            FanTimer.Start()
+        ElseIf dataDigital(0) = 251 Then
+            If fanCycle = 0 Then
+                FanPictureBox.BackColor = Color.Lime
+            Else
+                FanPictureBox.BackColor = Color.Gray
+            End If
+            overwriteAC = 1
+            FanTimer.Start()
+        ElseIf dataDigital(0) = 247 Then
+            FanPictureBox.BackColor = Color.Lime
+            HVACPictureBox.BackColor = Color.Gray
+        ElseIf dataDigital(0) = 239 Then
+
+        Else
+            fanCycle = 0
+            If systemTemp > CInt(CoolingSetpointTextBox.Text) + 2 And HeaterRadioButton.Checked Then
+                FanPictureBox.BackColor = Color.Lime
+                FanTimer.Start()
+            ElseIf systemTemp < CInt(HeaterSetpointTextBox.Text) - 2 And ACRadioButton.Checked Then
+                FanPictureBox.BackColor = Color.Lime
+                FanTimer.Start()
+            Else
+
+            End If
+
+        End If
+
+    End Sub
+
+    Private Sub FanTimer_Tick(sender As Object, e As EventArgs) Handles FanTimer.Tick
+
+        If overwriteHeater = 1 Then
+            fanCycle = 1
+            FanPictureBox.BackColor = Color.Gray
+            HVACPictureBox.BackColor = Color.IndianRed
+            HeaterRadioButton.Checked = True
+            If systemTemp < roomTemp And HeaterRadioButton.Checked Then
+                AlertPictureBox.BackColor = Color.Red
+            Else
+                AlertPictureBox.BackColor = Color.Gray
+            End If
+        ElseIf overwriteAC = 1 Then
+            fanCycle = 1
+            FanPictureBox.BackColor = Color.Gray
+            HVACPictureBox.BackColor = Color.Cyan
+            ACRadioButton.Checked = True
+            If systemTemp > roomTemp And ACRadioButton.Checked Then
+                AlertPictureBox.BackColor = Color.Red
+            Else
+                AlertPictureBox.BackColor = Color.Gray
+            End If
+        Else
+            If systemTemp > CInt(CoolingSetpointTextBox.Text) + 2 And HeaterRadioButton.Checked Then
+                FanPictureBox.BackColor = Color.Gray
+                HVACPictureBox.BackColor = Color.Cyan
+                ACRadioButton.Checked = True
+            ElseIf systemTemp < CInt(HeaterSetpointTextBox.Text) - 2 And ACRadioButton.Checked Then
+                FanPictureBox.BackColor = Color.Gray
+                HVACPictureBox.BackColor = Color.IndianRed
+                HeaterRadioButton.Checked = True
+            End If
+
+        End If
+        overwriteHeater = 0
+        overwriteAC = 0
+        FanTimer.Stop()
     End Sub
 
 End Class
